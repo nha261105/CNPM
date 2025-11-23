b5713d129c40de8de3b7e6d09e7d4b684b67e2cd
cuong9565 <cuongmanhle0905@gmail.com>
2025-11-23 21:31:16 +0700

    Cuong push update pickup point

--- backend/src/modules/pickup_point/pickup_point.service.ts
+++ backend/src/modules/pickup_point/pickup_point.service.ts
@@ -1,3 +1,4 @@
+import { json } from "zod";
 import supabase from "../../config/supabaseClient.js";
 
 export const getAllPickup = async () => {
@@ -7,5 +8,17 @@ export const getAllPickup = async () => {
     if (error) {
         throw new Error(`Error fetching pickup points: ${error.message}`);
     }
+    return data;
+}
+
+export const updatePickUp = async(pickup_point_id: number, latitude: number, longitude: number, description: string) => {
+    const newData = {
+        pickup_point_id: pickup_point_id,
+        latitude: latitude,
+        longitude: longitude,
+        description: description
+    }
+    const {data, error} = await supabase.from('pickup_point').update(newData).eq('pickup_point_id', pickup_point_id).select("pickup_point_id, latitude, longitude, description");
+    if(error) throw new Error(error.message)
     return data;
 }
