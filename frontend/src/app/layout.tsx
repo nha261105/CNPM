2559280f5d282b05f000c4b1f718813aff61dc5f
ThanhHai151 <thanhhai1512005@gmail.com>
2025-11-29 11:14:07 +0700

    2

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,13 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <AuthProvider>
-          {children}
-          <Toaster />
-        </AuthProvider>
-      </body>
-    </html>
+    <AuthProvider>
+      {children}
+      <Toaster />
+    </AuthProvider>
   );
 }
